a6814105a0f27aa9a7c9a58ce1c389c2266bba71
David Estes <destes@redwindsw.com>
2012-02-13 14:49:34 -0500

    added relationships and orders

--- app/controllers/spud/admin/products_controller.rb
+++ app/controllers/spud/admin/products_controller.rb
@@ -1,2 +1,60 @@
-class Spud::Admin::ProductsController < ApplicationController
+class Spud::Admin::ProductsController < Spud::Admin::ApplicationController
+	belongs_to_spud_app :products
+	before_filter :load_product,:only => [:edit,:update,:destroy,:show]
+	add_breadcrumb "Products", :spud_admin_products_path
+	def index
+		@products = SpudProduct.order(:name).paginate :page => params[:page]
+		respond_with @products
+	end
+
+	def new
+		add_breadcrumb "New", :new_spud_admin_product_path
+		@product = SpudProduct.new
+		respond_with @product
+	end
+
+	def create
+		add_breadcrumb "New", :new_spud_admin_product_path
+		@product = SpudProduct.new(params[:spud_product])
+		if @product.save
+			flash[:notice] = "Product successfully saved."
+		else
+			flash[:error] = "Error saving the product."
+		end
+
+		respond_with @product,:location => spud_admin_products_url
+	end
+
+	def edit
+		add_breadcrumb "Edit", :edit_spud_admin_product_path
+		respond_with @product
+	end
+
+	def update
+		add_breadcrumb "Edit", :edit_spud_admin_product_path
+		if @product.update_attributes(params[:spud_product])
+			flash[:notice] = "Product saved successfully"
+		else
+			flash[:error] = "Error saving this product!"
+		end
+		respond_with @product,:location => spud_admin_products_url
+	end
+
+	def destroy
+		if @product.destroy
+			flash[:notice] = "Product removed successfully!"
+		else
+			flash[:error] = "Error creating the product."
+		end
+		respond_with @product,:location => spud_admin_products_url
+	end
+
+private
+	def load_product
+		@product = SpudProduct.find(params[:id])
+		if @product.blank?
+			flash[:error] = "Product not found!"
+			redirect_to spud_admin_products_url() and return
+		end
+	end
 end
--- app/models/spud_product.rb
+++ app/models/spud_product.rb
@@ -1,6 +1,8 @@
 class SpudProduct < ActiveRecord::Base
 	searchable
 	has_many :spud_product_prices
+    accepts_nested_attributes_for :spud_product_prices, :reject_if => lambda { |a| a[:price_code].blank? }, :allow_destroy => true
+
 	validates :name,:presence => true,:uniqueness => true
 	# validates :item_number,:presence => true,:uniqueness => true,:if => 
 
